e6d83064dc17ad4bd5696e53afe2f50bc14ac359
Arif Khaishagi <69190118+Arif-Khaishagi@users.noreply.github.com>
2024-12-10 23:15:10 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,7 +3,4 @@
 export default{
     schema: "./utils/schema.js",
     dialect: 'postgresql',
-    dbCredentials: {
-        url: 'postgresql://neondb_owner:q6NU4uLxdWPr@ep-nameless-violet-a5w9rm4r.us-east-2.aws.neon.tech/neondb?sslmode=require'
-    }
 }
